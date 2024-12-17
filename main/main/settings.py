07694f34f5d76db61b8f8f52d08c9a62063a6198
lewis <rinatt116@gmail.com>
2024-12-18 02:55:21 +0300

    settings

--- main/main/settings.py
+++ main/main/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'uti_puti.apps.MainConfig'
+    'uti_puti.apps.UtiPutiConfig'
 ]
 
 MIDDLEWARE = [
--- main/main/urls.py
+++ main/main/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("uti_puti.urls"))
+    path("", include("uti_puti.urls")),
     path('admin/', admin.site.urls),
 ]
