0e29b11f71b117d34514d585c29da375b3bee773
lewis <rinatt116@gmail.com>
2024-12-18 02:56:50 +0300

    add models

--- main/uti_puti/models.py
+++ main/uti_puti/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CoursesGroup(models.Model):
+    name = models.CharField(max_length=200)
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=1000, default="")
+    pic_name = models.CharField(max_length=100)
+    video_name = models.CharField(max_length=100)
+    courses_group = models.ForeignKey(CoursesGroup, related_name='courses', on_delete=models.CASCADE, null=True)
