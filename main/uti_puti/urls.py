936489b43637db098e5e95e64ba1f439005169c6
lewis <rinatt116@gmail.com>
2024-12-19 15:01:11 +0300

    url to rates and prices

--- main/uti_puti/urls.py
+++ main/uti_puti/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("reg/", views.register_view, name="register"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile, name="profile"),
+    path("prices/", views.prices, name="prices"),
     path("courses/", views.courses, name="courses"),
-    path("courses/<int:course_id>", views.video_page, name="course_page"),
+    path("rates/", views.rates, name="rates"),
+    path("courses/<int:course_id>", views.video_page, name="course_page")
 ]
