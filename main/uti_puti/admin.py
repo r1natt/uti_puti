064af22994f747eb4cec978916549bdb1edd2843
lewis <rinatt116@gmail.com>
2024-12-19 15:01:26 +0300

    add view model to admin panel

--- main/uti_puti/admin.py
+++ main/uti_puti/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, CoursesGroup
+from .models import Course, CoursesGroup, View
 
 
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(CoursesGroup)
+admin.site.register(View)
